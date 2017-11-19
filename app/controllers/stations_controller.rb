a29816217b78c8f060f9edc4b65bd6df85cd77c7
Hiroyuki Imura <sinewave.440@gmail.com>
2017-11-19 09:00:36 +0000

    駅IDから路線情報を取得するAPIを作成した

--- app/controllers/stations_controller.rb
+++ app/controllers/stations_controller.rb
@@ -8,4 +8,14 @@ class StationsController < ApplicationController
     @station = Station.find(params[:id])
     render json: @station
   end
+
+  def lines
+    name = Station.find(params[:id]).name
+    lines = Station.where(name: name).select('line_id')
+
+    @lines = lines.map{|line|
+      Line.find(line.line_id)
+    }
+    render json: @lines
+  end
 end
