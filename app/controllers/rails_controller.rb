f4424a4f452b8e54d51db4e7fe0e8026ae434ae3
hiracky16 <h.piiice16@gmail.com>
2017-12-25 22:09:18 +0900

    line_id とその路線内での order で線路データを生成するよう修正

--- app/controllers/rails_controller.rb
+++ app/controllers/rails_controller.rb
@@ -1,25 +1,21 @@
 require 'JSON'
 class RailsController < ApplicationController
     def index
+        p request.body.read
         json_params = JSON.parse(request.body.read)
-        # line_id = json_params['line_id'].to_i
-        start_station = Station.find_by(id: json_params['start'].to_i)
-        end_station = Station.find_by_id(json_params['end'].to_i)
-        if start_station.nil? || end_station.nil? then
-            render json: {'message': 'No stations'}, status: 400
-            return
-        end
-        start_order = start_station['rail_order']
-        end_order = end_station['rail_order']
+        line_id = json_params['line_id'].to_i
+        start_order = json_params['start_order'].to_i
+        end_order = json_params['end_order'].to_i
         if start_order > end_order then
             start_order, end_order = end_order, start_order
         end
         order = Rail.arel_table[:order]
-        unless order then
+        @rails = Rail.where({'line_id': line_id})
+            .where(order.gteq(start_order).and(order.lteq(end_order)))
+        unless @rails then
             render json: {'message': 'No rails'}, status: 400
             return
         end
-        @rails = Rail.where(order.gteq(start_order).and(order.lteq(end_order)))
         render json: @rails
     end
 end
