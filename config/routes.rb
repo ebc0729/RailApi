6be9e4d2554c2f2210ca5389caee5a9834ebcb84
Hiroyuki Imura <sinewave.440@gmail.com>
2018-01-07 06:42:25 +0000

    エンドポイントを変更し、影響箇所を修正した。

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :areas
   resources :stations
-  resources :company
+  resources :companies
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
