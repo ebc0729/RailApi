bb2d0195093cfe0d62c4d1490856bb9680643f60
hiracky16 <h.piiice16@gmail.com>
2018-01-17 22:32:33 +0900

    routeの変更とコントローラの修正

--- app/controllers/rails_controller.rb
+++ app/controllers/rails_controller.rb
@@ -1,16 +1,15 @@
-require 'JSON'
+require 'json'
 class RailsController < ApplicationController
-    def generate
-        p request.body.read
+    def generates
         json_params = JSON.parse(request.body.read)
-        line_id = json_params['line_id'].to_i
+        route_id = json_params['route_id'].to_i
         start_order = json_params['start_order'].to_i
         end_order = json_params['end_order'].to_i
         if start_order > end_order then
             start_order, end_order = end_order, start_order
         end
         order = Rail.arel_table[:order]
-        @rails = Rail.where({'line_id': line_id})
+        @rails = Rail.where({'route_id': route_id})
             .where(order.gteq(start_order).and(order.lteq(end_order)))
         unless @rails then
             render json: {'message': 'No rails'}, status: 400
