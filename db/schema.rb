9d506da811b9076f22112c1e70b7fac1c973964a
hiracky16 <h.piiice16@gmail.com>
2018-08-13 10:37:38 +0900

    カラム名の変更 #24

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180804001510) do
+ActiveRecord::Schema.define(version: 20180813012950) do
 
   create_table "areas", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.string "name"
@@ -39,8 +39,8 @@ ActiveRecord::Schema.define(version: 20180804001510) do
   create_table "records", force: :cascade, options: "ENGINE=InnoDB DEFAULT CHARSET=utf8" do |t|
     t.bigint "user_id"
     t.bigint "route_id"
-    t.integer "start_order"
-    t.integer "end_order"
+    t.integer "start_station_id"
+    t.integer "end_station_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["route_id"], name: "index_records_on_route_id"
