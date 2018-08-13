388f7b448a6189d75d6f37efcc114637c7f77f14
hiracky16 <h.piiice16@gmail.com>
2018-08-13 10:44:09 +0900

    コントローラのの修正 #24

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -7,27 +7,25 @@ class RecordsController < ApplicationController
         if user.nil?
             render json: {'message': 'No User'}, status: 400 and return
         end
-        # TODO: ユーザ削除を考慮すべき
-        # User エンティティに論理削除済みか確かめるメソッドを作ってもらってから着手
         route_id = json_params['route_id'].to_i
         # 路線の存在確認
         route = Route.find_by(id: route_id)
         if route.nil?
             render json: {'message': 'No route'}, status: 400 and return
         end
-        # start_order, end_orderの確認
+        # start_stationとend_stationの取得
         start_order = json_params['start_order'].to_i
         end_order = json_params['end_order'].to_i
-        start_rail = Rail.find_by({route_id: route_id, rail_order: start_order})
-        end_rail = Rail.find_by({route_id: route_id, rail_order: end_order})
-        if start_rail.nil? || end_rail.nil?
+        start_station = Station.find_by({route_id: route_id, rail_order: start_order})
+        end_station = Station.find_by({route_id: route_id, rail_order: end_order})
+        if start_station.nil? || end_station.nil?
             render json: {'message': 'Invalid start_order or end_order'}, status: 400 and return
         end
         record_attr = {
             'user_id': user_id,
             'route_id': route_id,
-            'start_order': start_order,
-            'end_order': end_order
+            'start_station_id': start_station.id,
+            'end_station_id': end_station.id
         }
         record = Record.new
         record.attributes = record_attr
