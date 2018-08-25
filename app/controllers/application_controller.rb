e09f27d9961f581ab9f6d989066a5b90cd3443d7
domi10momo <hb.kmaia93@icloud.com>
2018-08-26 07:46:13 +0900

    タイムゾーンを東京に設定 #44

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
 class ApplicationController < ActionController::API
   include DeviseTokenAuth::Concerns::SetUserByToken
+  config.time_zone = 'Tokyo'
+  config.active_record.default_timezone = :local
 end
