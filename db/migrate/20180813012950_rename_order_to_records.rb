f2c6ee5ead51beddf9555772bfd2d7ebb5019ce3
hiracky16 <h.piiice16@gmail.com>
2018-08-29 22:01:52 +0900

    不要なマイグレートを削除 #24

--- db/migrate/20180813012950_rename_order_to_records.rb
+++
@@ -1,6 +0,0 @@
-class RenameOrderToRecords < ActiveRecord::Migration[5.1]
-  def change
-    rename_column :records, :start_order, :start_station_id
-    rename_column :records, :end_order, :end_station_id
-  end
-end
